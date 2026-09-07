9377fa061ec056c89633c8261f4e01403195a8eb
mikeyperes <mikeyperes@users.noreply.github.com>
2026-09-07 23:14:35 +0000

    Require role authorization on GPTZero routes

--- routes/gptzero.php
+++ routes/gptzero.php
@@ -3,7 +3,7 @@
 use hexa_package_gptzero\Http\Controllers\GptZeroController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware(['web', 'auth', 'locked', 'system_lock', 'two_factor', 'role'])->group(function () {
     Route::get('gptzero/settings', [GptZeroController::class, 'settings'])->name('gptzero.settings');
     Route::post('gptzero/settings', [GptZeroController::class, 'saveSettings'])->name('gptzero.settings.save');
     Route::post('gptzero/test', [GptZeroController::class, 'testConnection'])->name('gptzero.test');
